096bbc8245a41607a910254c184c50651a16c753
Excel <edehexcel2000@gmail.com>
2022-07-31 22:43:53 +0100

    corrected version

--- src/pages/homepage/Navbar/NavBar.jsx
+++ src/pages/homepage/Navbar/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import logo from '../../../images/1x/Artboard 1.png'
 import './navbar.css'
 
